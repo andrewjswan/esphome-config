ef63f9b37724f3e90f6c9434fb04b645125562ef
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-12-10 16:33:19 +0200

    Add brightness

--- includes/matrix_lamp/matrix_lamp.h
+++ includes/matrix_lamp/matrix_lamp.h
@@ -7,14 +7,18 @@
 
 class MatrixLamp
 {
+#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
   protected:
     bool random_settings = false;
+#endif // #if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
 
   public:
     MatrixLamp();
     ~MatrixLamp();
 
+#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
     void SetRandomSettings(bool b=false);
+#endif // #if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
     void ShowFrame(uint8_t CurrentMode, esphome::Color current_color, light::AddressableLight *p_it);
 };
 
@@ -29,11 +33,13 @@ MatrixLamp::~MatrixLamp()
   FreeLeds();
 }
 
+#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
 void MatrixLamp::SetRandomSettings(bool b)
 {
   random_settings = b;
   selectedSettings = b;
 }
+#endif // #if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
 
 void MatrixLamp::ShowFrame(uint8_t CurrentMode, esphome::Color current_color, light::AddressableLight *p_it)
 {
@@ -42,8 +48,15 @@ void MatrixLamp::ShowFrame(uint8_t CurrentMode, esphome::Color current_color, li
   if (currentMode != CurrentMode)
   {
     loadingFlag = true;
+#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
     selectedSettings = random_settings;
-    if (!selectedSettings)
+    if (random_settings)
+    {
+      float br = (float)modes[CurrentMode].Brightness / (float)255;
+      id(neopixel_led).turn_on().set_brightness(br).perform();
+    }
+    else
+#endif // #if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
     {
       id(fastled_speed).state == modes[CurrentMode].Speed;
       id(fastled_variant).state == modes[CurrentMode].Scale;
@@ -52,6 +65,9 @@ void MatrixLamp::ShowFrame(uint8_t CurrentMode, esphome::Color current_color, li
   currentMode = CurrentMode;
 
   effectsTick();
+
+  modes[CurrentMode].Speed = id(fastled_speed).state;
+  modes[CurrentMode].Scale = id(fastled_variant).state;
 
   for (int i = 0; i < p_it->size(); i++)
   {
